6ed3e34981650c6bc3df4a6e0c91f52af0fccc78
igor-provotorov <garynya@yandex.ru>
2019-03-10 20:54:19 +0300

    fix: fix card number bug

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import orders from '../data/orders.json';
+import users from '../data/users.json';
 //import 'companies' from '../data/companies.json';
-//import 'users' from '../data/users.json';
 
 export default (function () {
-  var table = document.getElementById("app");
-  table.innerHTML = 
+  const table = document.getElementById("app");
+
+  table.innerHTML =
   `<table border="1" cellspacing="0">
     <thead>
       <tr>
@@ -21,15 +22,13 @@ export default (function () {
     </tbody>
   </table>`;
 
-  // const formatDate = (str) => {
-  //   return `${new Date(str*1000).toLocaleDateString("ru")}, ${new Date(str*1000).toLocaleTimeString("ru")}`
-  // }
 
-  const formatCardNumber = (str) => {
-    return `${str.slice(0,2)}********${str.slice(12,16)}`
+  const formatCardNumber = (number) => {
+    let formatNumber = number.replace(/(?<=\d{2})\d(?=\d{4})/g, '*');
+    return formatNumber;
   }
 
-  const formattingDate = (seconds) => {
+  const formatDate = (seconds) => {
     let date = new Date(seconds*1000);
     let day = date.getDate();
     let month = date.getMonth() + 1;
@@ -40,8 +39,9 @@ export default (function () {
   const tbody = document.getElementById('tbody');
 
   for (let i = 0; i < orders.length; i++) {
-    let time = formattingDate(orders[i].created_at);
+    let time = formatDate(orders[i].created_at);
     let card = formatCardNumber(orders[i].card_number);
+
     tbody.innerHTML+=
     `<tr id="order_${orders[i].id}">
       <td>${orders[i].id}</td>
@@ -53,7 +53,5 @@ export default (function () {
       <td>${orders[i].order_country} (${orders[i].order_ip})</td>
     </tr>`;
   };
-
-
 
 }());
