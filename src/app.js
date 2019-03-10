1b8529de00ac54c0d226ebdda6561c7aca232f48
igor-provotorov <garynya@yandex.ru>
2019-03-10 20:32:46 +0300

    feat: complete task#1

--- src/app.js
+++ src/app.js
@@ -1,37 +1,59 @@
-// this is an example of improting data from JSON
 import orders from '../data/orders.json';
 //import 'companies' from '../data/companies.json';
 //import 'users' from '../data/users.json';
 
 export default (function () {
-    // YOUR CODE GOES HERE
-    // next line is for example only
-    var table = document.getElementById("app");
-	table.innerHTML = 
-	`<table border="1" cellspacing="0">
-		<thead>
-			<tr>
-				<th>Transaction ID</th>
-				<th>User Info</th>
-				<th>Order Date</th>
-				<th>Order Amount</th>
-				<th>Card Number</th>
-				<th>Card Type</th>
-				<th>Location</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Transaction ID</td>
-				<td>User Info</td>
-				<td>Order Date</td>
-				<td>Order Amount</td>
-				<td>Card Number</td>
-				<td>Card Type</td>
-				<td>Location</td>
-			</tr>
-		</tbody>
-	</table>`;
-	
-	console.dir(orders);
+  var table = document.getElementById("app");
+  table.innerHTML = 
+  `<table border="1" cellspacing="0">
+    <thead>
+      <tr>
+        <th>Transaction ID</th>
+        <th>User Info</th>
+        <th>Order Date</th>
+        <th>Order Amount</th>
+        <th>Card Number</th>
+        <th>Card Type</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody id="tbody">
+    </tbody>
+  </table>`;
+
+  // const formatDate = (str) => {
+  //   return `${new Date(str*1000).toLocaleDateString("ru")}, ${new Date(str*1000).toLocaleTimeString("ru")}`
+  // }
+
+  const formatCardNumber = (str) => {
+    return `${str.slice(0,2)}********${str.slice(12,16)}`
+  }
+
+  const formattingDate = (seconds) => {
+    let date = new Date(seconds*1000);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return `${day > 9 ? day : `0${day}` }/${month > 9 ? month : `0${month}`}/${year}, ${date.toLocaleTimeString("en-US")}`;
+  }
+
+  const tbody = document.getElementById('tbody');
+
+  for (let i = 0; i < orders.length; i++) {
+    let time = formattingDate(orders[i].created_at);
+    let card = formatCardNumber(orders[i].card_number);
+    tbody.innerHTML+=
+    `<tr id="order_${orders[i].id}">
+      <td>${orders[i].id}</td>
+      <td class="user_data">${orders[i].user_id}</td>
+      <td>${time}</td>
+      <td>$${orders[i].total}</td>
+      <td>${card}</td>
+      <td>${orders[i].card_type}</td>
+      <td>${orders[i].order_country} (${orders[i].order_ip})</td>
+    </tr>`;
+  };
+
+
+
 }());
